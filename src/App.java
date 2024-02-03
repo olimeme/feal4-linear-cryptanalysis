ce7ac01883540289c94605e79f4586daad31bbd6
olimeme <olisanmomyn@proton.me>
2024-02-03 16:18:08 +0000

    add task

--- src/App.java
+++ src/App.java
@@ -1,3 +1,22 @@
+// This assignment will involve implementing a linear cryptanalysis attack on the weak block 
+// cipher FEAL-4,as described in the course,to find the six secret sub-keys that have been used.
+// This is a VERY hard assignment and will take a considerable amount of time,so please start working 
+// on it as soon as possible.
+
+// The linear cryptanalysis attack can be implemented in the programming language of your choice.
+// Your program will have to loop through a lot of different possible values,so it should be reasonably
+//  efficient.The source code for the FEAL-4 cipher(from which the six secret sub-keys have been removed)
+//  in C and Java is provided below in the files FEAL.c and FEAL.java,so 
+//  you may wish to make use of this code and implement your attack in one of these languages.An executable 
+//  version of this code which has the secret key built into it was used to generate the 200 random 
+//  plaintext/ciphertext pairs which can be found below in the file known.txt.
+
+// Your task is to discover as many of the bits as possible of the six 32-bit sub-keys K0-K5 used in this
+//  cipher.The more bits,the more marks you will get.However,you will get some marks for even finding a few
+//   bits of the sub-keys,as this is a very difficult task.You should submit your code along with a written 
+//   report describing how you went about the cryptanalysis and the results obtained through the Loop page 
+//   for this module by 10 am Monday 29 th July.
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
